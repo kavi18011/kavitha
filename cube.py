268e26cd52248b37dfde409398df69db9fdd201c
unknown <guvvalakavi11801@gmail.com>
2019-11-04 09:49:34 +0530

    first

---
+++ cube.py
@@ -0,0 +1,43 @@
+# numbers=[]
+# squares=[]
+# cubes=[]
+#
+#
+# start=1
+# end=10
+# for count in range(start,end+1):
+#     numbers.append(count)
+#     squares.append(count**2)
+#     cubes.append(count**3)
+# print(numbers)
+# print(cubes)
+# print(squares)
+
+
+# wap cub the list
+
+# cubes=[]
+# # start=1
+# # end=10
+#
+# for i in range(1,5+1):
+#     cubes.append(i**3)
+# print("cubes",cubes)
+
+# squares program
+# squares=[]
+# for i in range(1,4+1):
+#     squares.append(i**2)
+# print("squares",squares)
+
+
+# numbers in list
+# numbers=[]
+# for i in range(1,4+1):
+#     numbers.append(i)
+# print(numbers)
+
+cube=[]
+for i in range(1,5+1):
+    cube.append(i**3)
+print("cube",cube)
