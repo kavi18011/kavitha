d0a6acb243a4f28034ba5d1d17d7c937be0c9dd6
unknown <guvvalakavi11801@gmail.com>
2019-11-04 09:57:29 +0530

    sunny

---
+++ anagram.py
@@ -0,0 +1,22 @@
+# def isAnagram(str1,str2):
+#     return sorted(str1)==sorted(str2)
+# str1="star"
+# str2="rats"
+# if isAnagram(str1,str2):
+#     print("strings is anagram")
+# else:
+#     print("strings is not anagram")
+str1="star"
+str2="atrs"
+if sorted(str1)==sorted(str2):
+    print("strings is anagram")
+else:
+    print("strings is not anagram")
+
+# list=[1,2,3,4,5,6]
+# lis2=list.index(list[-1])+1
+# print(lis2)
+#
+#
+# #can we assign dict as key in dict, if not why A) we can not assign dict as key because keys must me immuatable, dict is mutable
+# d={"a":1,"b":2,"c":4}
